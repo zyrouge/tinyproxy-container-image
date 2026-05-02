f85c3c55c20ad6cb06ec7db8d338033f5cd72465
Zyrouge <hello@zyrouge.me>
2026-05-02 17:27:13 +0530

    ci: fix release check

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ TINYPROXY_GIT_URL="https://github.com/tinyproxy/tinyproxy"
 if [ -z "${TINYPROXY_VERSION:-}" ]; then
     TINYPROXY_VERSION=$(gh release list -R "$TINYPROXY_GIT_URL" --exclude-drafts --exclude-pre-releases -L 1 --json tagName | jq -r '.[0].tagName')
     if [ "$?" -ne 0 ] || [ -z "$TINYPROXY_VERSION" ]; then
+        echo "Failed to get determine version"
         exit 1
     fi
 fi
@@ -14,6 +15,7 @@ fi
 if [ -z "${TINYPROXY_REVISION:-}" ]; then
     TINYPROXY_REVISION=$(git ls-remote --tags --refs "$TINYPROXY_GIT_URL" "refs/tags/$TINYPROXY_VERSION" | cut -f1)
     if [ "$?" -ne 0 ] || [ -z "$TINYPROXY_REVISION" ]; then
+        echo "Failed to determine revision"
         exit 1
     fi
 fi
--- needs_release.sh
+++ needs_release.sh
@@ -4,6 +4,7 @@ set -eu
 
 GIT_URL="https://github.com/zyrouge/tinyproxy-container-image.git"
 TINYPROXY_GIT_URL="https://github.com/tinyproxy/tinyproxy"
+IMAGE_NAME="ghcr.io/zyrouge/tinyproxy"
 
 if [ -z "${TINYPROXY_VERSION:-}" ]; then
     TINYPROXY_VERSION="$(gh release list -R "$TINYPROXY_GIT_URL" --exclude-drafts --exclude-pre-releases -L 1 --json tagName --jq '.[0].tagName')"
@@ -12,8 +13,8 @@ if [ -z "${TINYPROXY_VERSION:-}" ]; then
     fi
 fi
 
-FOUND_VERSION=$(gh release view -R "$GIT_URL" "$TINYPROXY_VERSION" --json tagName --jq '.[0].tagName' 2>&1 || echo "")
-if [ "$FOUND_VERSION" != "release not found" ]; then
+FOUND_VERSION=$(docker manifest inspect "$IMAGE_NAME:$TINYPROXY_VERSION" > /dev/null 2>&1 && echo "found" || echo "")
+if [ "$FOUND_VERSION" = "found" ]; then
     exit 1
 fi
 
